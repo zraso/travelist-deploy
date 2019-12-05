16fd2374aff5f894a2445546b6855307627b0c45
zraso <zaira.rasool@hotmail.co.uk>
2019-12-05 15:06:33 +0000

    server edit4

--- server.js
+++ server.js
@@ -52,8 +52,8 @@ mongoose
   });
 
 if (process.env.NODE_ENV === 'production') {
-  app.use(express.static(path.join(__dirname, 'client/build')));
+  app.use(express.static(path.join(__dirname, 'client/public')));
   app.get('/*', function (req, res) {
-    res.sendFile(path.join(__dirname, 'client/build', 'index.html'));
+    res.sendFile(path.join(__dirname, 'client/public', 'index.html'));
   });
 }
