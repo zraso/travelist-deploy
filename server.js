1180b85f5c772c92c62a6cd8922270d9e1a9d15c
zraso <zaira.rasool@hotmail.co.uk>
2019-12-05 15:05:24 +0000

    server edit3

--- server.js
+++ server.js
@@ -53,7 +53,7 @@ mongoose
 
 if (process.env.NODE_ENV === 'production') {
   app.use(express.static(path.join(__dirname, 'client/build')));
-  app.get('*', function (req, res) {
+  app.get('/*', function (req, res) {
     res.sendFile(path.join(__dirname, 'client/build', 'index.html'));
   });
 }
